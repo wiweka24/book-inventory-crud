af1a803fcb042df183efec7fd3e92a3a59c8ad34
ricomalangi <rikomalangi@gmail.com>
2022-03-08 13:41:20 +0800

    menambahkan fitur login dan beberapa fitur lainnya

--- src/main/java/bookinventory/crud/repository/UserRepository.java
+++ src/main/java/bookinventory/crud/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package bookinventory.crud.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import bookinventory.crud.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long>{
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    User findByEmail(String email);
 }
     
