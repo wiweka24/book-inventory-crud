2522cdefbfb4b166f01376a263cad565be86e86b
wiweka <yogomu24@gmail.com>
2022-03-11 10:07:13 +0700

    menghapus import tidak diperlukan

--- src/main/java/bookinventory/crud/CustomUserDetails.java
+++ src/main/java/bookinventory/crud/CustomUserDetails.java
@@ -5,11 +5,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 public class CustomUserDetails implements UserDetails {
 
